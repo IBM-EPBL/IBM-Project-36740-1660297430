76d79939a00d9f9af3de36baa8383f1b180eca18
Praveenkumar1922002 <86066682+Praveenkumar1922002@users.noreply.github.com>
2022-11-12 09:29:35 -0800

    Add files via upload

---
+++ Implementing Web Application/Create IBM DB2 and connect with Python/SangeethKumar P/db_con.py
@@ -0,0 +1,11 @@
+
+from flask import Flask, render_template
+from cgi import print_form
+import ibm_db
+
+conn = ibm_db.connect("DATABASE=bludb;HOSTNAME=3883e7e4-18f5-4afe-be8c-fa31c41761d2.bs2io90l08kqb1od8lcg.databases.appdomain.cloud;PORT=31498;SECURITY=SSL;SSLServerCertificate=DigiCertGlobalRootCA.crt;UID=bvx19292;PWD=yDuuJH7Oqdzbnxnk;", "", "")
+app = Flask(__name__)
+
+print(conn)
+print('Connected Successfully...')
+
